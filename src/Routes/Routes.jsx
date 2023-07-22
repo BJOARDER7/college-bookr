bc106b95aeff59f79a25278321fe334cc2232a81
BJoarder <bjoarder87@gmail.com>
2023-07-22 11:49:09 +0600

    college project setup

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,10 @@
+const { createBrowserRouter } = require("react-router-dom");
+const { default: Main } = require("../layout/Main/Main");
+const { default: Home } = require("../pages/Home/Home");
+
+
+
+
+
+export default router;
+
